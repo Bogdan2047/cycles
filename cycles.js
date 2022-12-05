6f7991948916f4cb825e53d3c7cc55888785921e
Bogdan2047 <81803193+Bogdan2047@users.noreply.github.com>
2022-12-05 12:28:42 +0200

    attempt

---
+++ cycles.js
@@ -0,0 +1,43 @@
+// let arr = []
+
+// for (let i = 0; i <= 200; i++) {
+    
+//     if(i % 3 === 0){
+//         arr[i] = i;
+//         console.log(arr);
+//     }
+// }
+// console.log(arr);
+
+
+// let arr2 = []
+
+// for (let i = 0; i <= 10; i++) {
+//     arr2[i] = i * i;
+// }
+// console.log(arr2);
+
+// const word = "hello";
+
+// var arr2 = [];
+// for (i in word) {
+//   if (arr2[word[i]] != undefined) {
+//     (arr2[word[i]] ++)
+//   } else {
+//     (arr2[word[i]] = 1)
+//   }
+// }
+// console.log(arr2);
+
+// const word2 = "hello word"
+
+// var arr3 = [];
+// for (i in word2) {
+//   if (arr3[word2[i]] != undefined) {
+//     (arr3[word2[i]] ++)
+//   } else {
+//     (arr3[word2[i]] = 1)
+//   }
+// }
+// console.log(arr3);
+
